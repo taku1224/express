e397009f5025855b7433bd3edf74476b0a47f473
taku1224 <2101100190or@cyber-u.ac.jp>
2023-12-07 01:04:23 +0900

    dog api

--- routes/dog.js
+++ routes/dog.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const request = require('request');
 
 router.get('/', async(req, res) => {
-request('https://api.qrserver.com/v1/create-qr-code/?data=イケてないコード&size=100x100', function(error, response, body){
+request('https://dog.ceo/api/breeds/image/random', function(error, response, body){
 if (!error && response.statusCode == 200){
 const data = JSON.parse(body);
 res.json(data);
